fec2211968aaf021ad8fe46ad9ed2888749e292f
meilanaafifm <a710230056@student.ums.ac.id>
2026-01-06 23:44:38 +0700

    Fix skip-link accessibility CSS

--- prodi_website/wsgi.py
+++ prodi_website/wsgi.py
@@ -1,10 +1,14 @@
 """
 WSGI config for prodi_website project.
 """
-
 import os
+import sys
+
+path = '/home/meilanaafif2405/project_akhir'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'prodi_website.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prodi_website.settings')
-
 application = get_wsgi_application()
